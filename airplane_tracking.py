5947cf1df83cb172fb0e240e27e7f1b5b5a14402
abrartarafder <95165049+abrartarafder@users.noreply.github.com>
2022-08-01 20:52:14 -0400

    Create airplane_tracking.py

---
+++ airplane_tracking.py
@@ -0,0 +1,20 @@
+import requests
+
+params = {
+  'access_key': '_________________________________'
+}
+
+api_result = requests.get('http://api.aviationstack.com/v1/flights', params)
+
+api_response = api_result.json()
+
+name = input("Enter an Airline name: ")
+for i in range(100):
+    given_name = api_response['data'][i]['airline']['name']
+    departure_airport = api_response['data'][i]['departure']['airport']
+    arrival_airport = api_response['data'][i]['arrival']['airport']
+    flight_number = api_response['data'][i]['flight']['number']
+    given_code = api_response['data'][i]['airline']['iata']
+
+    if name == given_name:
+        print(f'\nDeparture Airport: {departure_airport}\nArrival Airport: {arrival_airport}\nFlight Number: {given_code}{flight_number}')
