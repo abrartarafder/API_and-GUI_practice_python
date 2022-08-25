33215bbaf53a6435ae6247e24b66ecdfb857c42e
abrartarafder <95165049+abrartarafder@users.noreply.github.com>
2022-08-24 20:17:13 -0400

    Update airplane_tracking.py

--- airplane_tracking.py
+++ airplane_tracking.py
@@ -1,7 +1,7 @@
 import requests
-# because the API shows up to 100 different flights -- range is 101 (Starts at 0 ends at 100 (not including 101))
+
 params = {
-  'access_key': '_________________________________'
+  'access_key': '2aafab93964f1bd7fbe4c41b8bdd6aa4'
 }
 
 api_result = requests.get('http://api.aviationstack.com/v1/flights', params)
@@ -9,7 +9,8 @@ api_result = requests.get('http://api.aviationstack.com/v1/flights', params)
 api_response = api_result.json()
 
 name = input("Enter an Airline name: ")
-for i in range(101):
+# 100 responses so range 100 including 0
+for i in range(100):
     given_name = api_response['data'][i]['airline']['name']
     departure_airport = api_response['data'][i]['departure']['airport']
     arrival_airport = api_response['data'][i]['arrival']['airport']
@@ -17,4 +18,4 @@ for i in range(101):
     given_code = api_response['data'][i]['airline']['iata']
 
     if name == given_name:
-        print(f'\nDeparture Airport: {departure_airport}\nArrival Airport: {arrival_airport}\nFlight Number: {given_code}{flight_number}')
+        print(f'\nDeparture Airport: {departure_airport}\nArrival Airport: {arrival_airport}\nFlight Number: {given_code}{flight_number}\n')
